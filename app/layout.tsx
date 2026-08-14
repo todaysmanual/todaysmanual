90b81ab10a69a0dea365807fa818ae0f303ef925
PanAvest <panavest5@gmail.com>
2026-08-14 12:21:55 +0000

    Keep CMS metadata dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { getContentBundle } from "@/lib/cms/content";
 import "./globals.css";
 
+export const dynamic = "force-dynamic";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
